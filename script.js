b9c5a18e32c41d9f2e79aea9e05666c117cd4726
meyazhaganraajan <109955520+meyazhaganraajan@users.noreply.github.com>
2023-01-08 15:03:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let container = document.createElement("div");
+container.classList.add("conatiner");
+
+let span = document.getElementById("span");
+span.innerHTML = 10;
+
+document.body.append(container);
+let count = 10;
+setTimeout(() => {
+  count = count - 1;
+  span.innerHTML = count;
+  setTimeout(() => {
+    count = count - 1;
+    span.innerHTML = count;
+    setTimeout(() => {
+      count = count - 1;
+      span.innerHTML = count;
+      setTimeout(() => {
+        count = count - 1;
+        span.innerHTML = count;
+        setTimeout(() => {
+          count = count - 1;
+          span.innerHTML = count;
+          setTimeout(() => {
+            count = count - 1;
+            span.innerHTML = count;
+            setTimeout(() => {
+              count = count - 1;
+              span.innerHTML = count;
+              setTimeout(() => {
+                count = count - 1;
+                span.innerHTML = count;
+                setTimeout(() => {
+                  count = count - 1;
+                  span.innerHTML = count;
+                  setTimeout(() => {
+                    count = count - 1;
+                    span.innerHTML = "Happy Independence Day";
+                    span.classList.add("Independence");
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
